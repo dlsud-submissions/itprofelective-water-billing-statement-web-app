5b2085c606cb3fe50c1c03b727f31ef8d41ad864
cH0NKIIs34L <chonkyseal.work@gmail.com>
2026-07-06 09:11:38 +0800

    feat: validate water consumption input

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 // Get DOM elements
 const statementForm = document.getElementsByClassName('statement-form');
 const customerNameInput = document.querySelector('#customer-name-input');
-const waterConsumptionInput = document.getElementsByName('water-consumption-input');
+const waterConsumptionInput = document.querySelector('#water-consumption-input');
 const customerTypeSelect = document.getElementsByName('customer-type-select');
 const submitBtn = document.getElementById('submit-btn');
 const resetBtn = document.getElementById('reset-btn');
@@ -18,22 +18,22 @@ let validateFormField = (e) => {
         case 'customer-name-input':
             validateCustomerName(inputValue);
             break;
+        case 'water-consumption-input':
+            validateWaterConsumption(inputValue);
+            break;
         default:
             console.log('default');
     }
 }
 
-const validateCustomerName = (inputValue) => {
-    // Validate input value
-    let inputIsValid = inputValue !== '';
-
-    const spanError = document.getElementById('customer-name-error')
-    if (!inputIsValid) {
-        spanError.innerText = 'Customer Input is a required field';
+let triggerSpanError = (command='removeError') => {
+    if (command === 'setError') {
         spanError.classList.add('error-span');
 
         submitBtn.setAttribute('disabled', 'true');
-    } else {
+    } 
+    
+    if (command === 'removeError') {
         spanError.innerText = '';
         spanError.classList.remove('error-span');
 
@@ -41,7 +41,34 @@ const validateCustomerName = (inputValue) => {
     }
 }
 
+const validateCustomerName = (inputValue) => {
+    // Validate input value
+    let inputIsEmpty = inputValue === '';
+
+    const spanError = document.getElementById('customer-name-error')
+    if (inputIsEmpty) {
+        spanError.innerText = 'Customer Input is a required field';
+        triggerSpanError('setError');
+    } else {
+        triggerSpanError('removeError');
+    }
+}
+
+const validateWaterConsumption = (inputValue) => {
+    const spanError = document.getElementById('water-consumption-error');
+    
+    
+    let negativeInput = inputValue < 1;
+    if (negativeInput) {
+        spanError.innerText = 'Must be a value greater than 0';
+        triggerSpanError('setError');
+    }
+
+    const rate = calculateRate(inputValue);
+}
+
 // Attach event listeners to input elements
 customerNameInput.addEventListener('blur', validateFormField);
+waterConsumptionInput.addEventListener('blur', validateFormField)
 
 // Attach event listener to submit button
