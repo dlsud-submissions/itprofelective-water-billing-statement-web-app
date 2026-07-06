3b923018d1dc91061f320fd93d0fce50ee4b6dc8
cH0NKIIs34L <chonkyseal.work@gmail.com>
2026-07-06 08:54:34 +0800

    feat: add validation function for customer name

--- src/script.js
+++ src/script.js
@@ -1,14 +1,35 @@
 // Get DOM elements
 const statementForm = document.getElementsByClassName('statement-form');
-const customerNameInput = document.getElementsByName('customer-name-input');
+const customerNameInput = document.querySelector('#customer-name-input');
 const waterConsumptionInput = document.getElementsByName('water-consumption-input');
 const customerTypeSelect = document.getElementsByName('customer-type-select');
 const submitBtn = document.getElementById('submit-btn');
 const resetBtn = document.getElementById('reset-btn');
 const outputBox = document.getElementsByClassName('output-box');
 
-// Check if all input fields have content
+let validateCustomerName = (e) => {
 
-// Validate input values
+    // Check for content
+    const inputValue = customerNameInput.value;
+
+    // Validate input  value
+    let inputIsValid = inputValue !== '';
+
+    const spanError = document.getElementById('customer-name-error')
+    if (!inputIsValid) {
+        spanError.innerText = 'Customer Input is a required field';
+        spanError.classList.add('error-span');
+
+        submitBtn.setAttribute('disabled', 'true');
+    } else {
+        spanError.innerText = '';
+        spanError.classList.remove('error-span');
+
+        submitBtn.setAttribute('disabled', 'false');
+    }
+}
+
+// Attach event listeners to input elements
+submitBtn.addEventListener('click', validateCustomerName);
 
 // Attach event listener to submit button
